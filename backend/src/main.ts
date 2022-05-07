089278a266a7a626c8fabf8d676e4f6717026784
Mohamed Awwad <mohamedawwad578@gmail.com>
2022-05-07 06:19:39 +0200

    modifying the backend files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from './modules/config/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './modules/app/app.module';
+import { AppLogger } from './modules/app/app.logger';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as cors from 'cors';
 import { TransformInterceptor } from './modules/common/interceptors/TransformInterceptor';
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
-import { AppModule } from 'modules/app/app.module';
-import { AppLogger } from 'modules/app/app.logger';
-import { ConfigService } from 'modules/config/config.service';
 
 async function bootstrap() {
   const logger = new AppLogger();
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
