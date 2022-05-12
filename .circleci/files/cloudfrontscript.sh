ef7a85ba83d588fabc2fc37adcf684486dfdd53c
Mohamed Awwad <mohamedawwad578@gmail.com>
2022-05-12 12:28:09 +0200

    Adding cloudfront job

--- .circleci/files/cloudfrontscript.sh
+++ .circleci/files/cloudfrontscript.sh
@@ -3,5 +3,5 @@
 aws cloudformation deploy \
 --template-file cloudfront.yml \
 --stack-name InitialStack \
---parameter-overrides WorkflowID=fmhfuxkubhogpqtzx \
+--parameter-overrides WorkflowID=wwzjqqprixkmdmbqj \
 --region us-west-2
