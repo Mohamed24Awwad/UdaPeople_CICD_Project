2925ed2f23b8edd51044d2f2992898d9bdc9bd50
Mohamed Awwad <mohamedawwad578@gmail.com>
2022-05-13 03:33:28 +0200

    Running All Jobs

--- .circleci/files/cloudfrontscript.sh
+++ .circleci/files/cloudfrontscript.sh
@@ -3,5 +3,5 @@
 aws cloudformation deploy \
 --template-file cloudfront.yml \
 --stack-name InitialStack \
---parameter-overrides WorkflowID=wwzjqqprixkmdmbqj \
+--parameter-overrides WorkflowID=gs05i3l9dr \
 --region us-west-2
